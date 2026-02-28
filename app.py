0c1d282fb231fedcc69ab920a022f670968ef3a7
kavyapriyavilasam20-max <kavyapriyavilasam20@gmail.com>
2026-02-28 14:46:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import pdfplumber  # better PDF text extraction
 # ----------------------
 # Function to extract text from PDF
 # ----------------------
+import PyPDF2
+
 def extract_text_from_pdf(pdf_file):
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
     text = ""
-    with pdfplumber.open(pdf_file) as pdf:
-        for page in pdf.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text += page_text + " "
+    for page in pdf_reader.pages:
+        text += page.extract_text() + " "
     return text
 
 # ----------------------
